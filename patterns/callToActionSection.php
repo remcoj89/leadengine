32bde23df8da02964f58cbd17403614f0d387f57
remcojonk <remcojonk@hotmail.com>
2026-01-06 11:19:55 +0700

    finished the desktop version of the home page

---
+++ patterns/callToActionSection.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Title: Call To Action
+ * Slug: leadmagnet/call-to-action-section
+ * Categories: Banners
+ * Inserter: true
+ */
+?>
+
+<!-- wp:group {"metadata":{"name":"call to action section"}, "style":{"spacing":{"padding":{"top":"var:preset|spacing|8","bottom":"var:preset|spacing|8"}}},"layout":{"type":"constrained","contentSize":"","wideSize":""}} -->
+<div class="wp-block-group container" style="padding-top:var(--wp--preset--spacing--8);padding-bottom:var(--wp--preset--spacing--8)"><!-- wp:group {"className":"flex-group, align-center","style":{"spacing":{"blockGap":"var:preset|spacing|6"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group flex-group, align-center"><!-- wp:group {"metadata":{"name":"text-group"},"className":"text-column","layout":{"type":"constrained"}} -->
+<div class="wp-block-group text-column"><!-- wp:heading {"textAlign":"right"} -->
+<h2 class="wp-block-heading has-text-align-right">Klaar om de eerste stap te zetten?</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"right"} -->
+<p class="has-text-align-right">Begin met een gratis intake of vul de Physis vragenlijst in. Zo zie je meteen waar jouw kansen liggen voor een duurzaam herstel</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"400"}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="http://leadengine.local/over-mij/" style="font-style:normal;font-weight:400;text-transform:uppercase">lees meer</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
+
+<!-- wp:image {"id":40,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="http://leadengine.local/wp-content/uploads/2025/12/klaar-voor-de-lijfstijl-819x1024.webp" alt="Vol energie op het strand tijdens de zonsondergang" class="wp-image-40"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ patterns/reccomendetArticles.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Title: Reccomendet Articles
+ * Slug: leadmagnet/recommended-articles-section
+ * Categories: Banners
+ * Inserter: true
+ */
+?>
+
+<!-- wp:group {"metadata":{"name":"recommended articles"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|8","bottom":"var:preset|spacing|8"}}},"backgroundColor":"soft-linen","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-soft-linen-background-color has-background" style="padding-top:var(--wp--preset--spacing--8);padding-bottom:var(--wp--preset--spacing--8)"><!-- wp:group {"metadata":{"name":"section Title"},"layout":{"type":"constrained","contentSize":"65%","wideSize":"100%"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|jet-black"}}}},"textColor":"jet-black"} -->
+<h2 class="wp-block-heading has-text-align-center has-jet-black-color has-text-color has-link-color">Wil je alvast wat practische tips?</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Laat je inspireren door inzichten en handvatten van de Lijfstijl. Simpele stappen en waardevolle tips die jouw herstel ondersteunen.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"padding":{"right":"0","left":"0","bottom":"0","top":"0"},"blockGap":"0"},"border":{"radius":{"topLeft":"16px","topRight":"16px","bottomLeft":"16px","bottomRight":"16px"}}},"backgroundColor":"white","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-white-background-color has-background" style="border-top-left-radius:16px;border-top-right-radius:16px;border-bottom-left-radius:16px;border-bottom-right-radius:16px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:image {"id":50,"width":"721px","height":"auto","aspectRatio":"1.7777777777777777","scale":"cover","sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large is-resized"><img src="http://leadengine.local/wp-content/uploads/2025/12/voeding-1024x683.webp" alt="gezonde voeding" class="wp-image-50" style="aspect-ratio:1.7777777777777777;object-fit:cover;width:721px;height:auto"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|2","bottom":"var:preset|spacing|2","left":"var:preset|spacing|2","right":"var:preset|spacing|2"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--2);padding-right:var(--wp--preset--spacing--2);padding-bottom:var(--wp--preset--spacing--2);padding-left:var(--wp--preset--spacing--2)"><!-- wp:heading {"level":3,"fontSize":"md"} -->
+<h3 class="wp-block-heading has-md-font-size">Het 6 domeinen model: Een brede kijk op gezondheid en herstel</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|jet-black"}}}},"textColor":"jet-black","fontSize":"sm"} -->
+<p class="has-jet-black-color has-text-color has-link-color has-sm-font-size">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"right","style":{"elements":{"link":{"color":{"text":"var:preset|color|rose-punch"}}}},"textColor":"rose-punch"} -->
+<p class="has-text-align-right has-rose-punch-color has-text-color has-link-color"><a href="http://leadengine.local/" data-type="page" data-id="19">Lees meer... </a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"0","left":"0","bottom":"0","top":"0"},"blockGap":"0"},"border":{"radius":{"topLeft":"16px","topRight":"16px","bottomLeft":"16px","bottomRight":"16px"}}},"backgroundColor":"white","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-white-background-color has-background" style="border-top-left-radius:16px;border-top-right-radius:16px;border-bottom-left-radius:16px;border-bottom-right-radius:16px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:image {"id":50,"width":"721px","height":"auto","aspectRatio":"1.7777777777777777","scale":"cover","sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large is-resized"><img src="http://leadengine.local/wp-content/uploads/2025/12/voeding-1024x683.webp" alt="gezonde voeding" class="wp-image-50" style="aspect-ratio:1.7777777777777777;object-fit:cover;width:721px;height:auto"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|2","bottom":"var:preset|spacing|2","left":"var:preset|spacing|2","right":"var:preset|spacing|2"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--2);padding-right:var(--wp--preset--spacing--2);padding-bottom:var(--wp--preset--spacing--2);padding-left:var(--wp--preset--spacing--2)"><!-- wp:heading {"level":3,"fontSize":"md"} -->
+<h3 class="wp-block-heading has-md-font-size">Het 6 domeinen model: Een brede kijk op gezondheid en herstel</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|jet-black"}}}},"textColor":"jet-black","fontSize":"sm"} -->
+<p class="has-jet-black-color has-text-color has-link-color has-sm-font-size">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"right","style":{"elements":{"link":{"color":{"text":"var:preset|color|rose-punch"}}}},"textColor":"rose-punch"} -->
+<p class="has-text-align-right has-rose-punch-color has-text-color has-link-color"><a href="http://leadengine.local/" data-type="page" data-id="19">Lees meer...</a> </p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"0","left":"0","bottom":"0","top":"0"},"blockGap":"0"},"border":{"radius":{"topLeft":"16px","topRight":"16px","bottomLeft":"16px","bottomRight":"16px"}}},"backgroundColor":"white","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-white-background-color has-background" style="border-top-left-radius:16px;border-top-right-radius:16px;border-bottom-left-radius:16px;border-bottom-right-radius:16px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:image {"id":50,"width":"721px","height":"auto","aspectRatio":"1.7777777777777777","scale":"cover","sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large is-resized"><img src="http://leadengine.local/wp-content/uploads/2025/12/voeding-1024x683.webp" alt="gezonde voeding" class="wp-image-50" style="aspect-ratio:1.7777777777777777;object-fit:cover;width:721px;height:auto"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|2","bottom":"var:preset|spacing|2","left":"var:preset|spacing|2","right":"var:preset|spacing|2"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--2);padding-right:var(--wp--preset--spacing--2);padding-bottom:var(--wp--preset--spacing--2);padding-left:var(--wp--preset--spacing--2)"><!-- wp:heading {"level":3,"fontSize":"md"} -->
+<h3 class="wp-block-heading has-md-font-size">Het 6 domeinen model: Een brede kijk op gezondheid en herstel</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|jet-black"}}}},"textColor":"jet-black","fontSize":"sm"} -->
+<p class="has-jet-black-color has-text-color has-link-color has-sm-font-size">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"right","style":{"elements":{"link":{"color":{"text":"var:preset|color|rose-punch"}}}},"textColor":"rose-punch"} -->
+<p class="has-text-align-right has-rose-punch-color has-text-color has-link-color"><a href="http://leadengine.local/" data-type="page" data-id="19">Lees meer... </a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
