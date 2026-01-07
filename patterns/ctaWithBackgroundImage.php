2a70ea696e4e06ab3f8471711a4554e7b84ab44f
remcojonk <remcojonk@hotmail.com>
2026-01-07 16:17:33 +0700

    created the about me section in the salespage template

---
+++ patterns/ctaWithBackgroundImage.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Title: CTA Section With Background Image
+ * Slug: leadmagnet/cta-section-bg-image
+ * Categories: banner
+ * Inserter: true
+ */
+?>
+
+<!-- wp:group {"metadata":{"name":"ctaSectionWithBgImage"},"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}},"background":{"backgroundImage":{"url":"http://leadengine.local/wp-content/uploads/2025/12/trajecten-lijfstijl-scaled.webp","id":49,"source":"file","title":"trajecten-lijfstijl"},"backgroundSize":"cover","backgroundPosition":"28.999999999999996% 77%"}},"layout":{"type":"constrained","wideSize":"100vw"}} -->
+<div class="wp-block-group" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"style":{"color":{"background":"#f5f0e6d9"},"spacing":{"padding":{"top":"var:preset|spacing|8","bottom":"var:preset|spacing|8"}},"elements":{"link":{"color":{"text":"var:preset|color|jet-black"}}}},"textColor":"jet-black","layout":{"type":"constrained","wideSize":"23rem"}} -->
+  <div class="wp-block-group has-jet-black-color has-text-color has-background has-link-color" style="background-color:#f5f0e6d9;padding-top:var(--wp--preset--spacing--8);padding-bottom:var(--wp--preset--spacing--8)"><!-- wp:heading {"textAlign":"center"} -->
+    <h2 class="wp-block-heading has-text-align-center">Twijfel je nog of dit bij je past?</h2>
+    <!-- /wp:heading -->
+
+    <!-- wp:paragraph {"align":"center"} -->
+    <p class="has-text-align-center">Na de eerste sessie beslis je of je  <br> doorgaat. Geen goed gevoel?  <br><strong>Stoppen kan kostenloos.</strong></p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+    <div class="wp-block-buttons"><!-- wp:button {"style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"400"}}} -->
+      <div class="wp-block-button">
+        <a class="wp-block-button__link wp-element-button" style="font-style:normal;font-weight:400;text-transform:uppercase">vrijblijvend proberen</a>
+      </div>
+      <!-- /wp:button -->
+    </div>
+    <!-- /wp:buttons -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
