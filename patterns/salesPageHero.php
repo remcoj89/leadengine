4a0ae7ff04fa5908ca0fdc8fe4893c4c7836903c
remcojonk <remcojonk@hotmail.com>
2026-01-07 13:13:45 +0700

    created the salesInfoCard block for the trajecten page

---
+++ patterns/salesPageHero.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Title: Sales Hero – Split (Showit style)
+ * Slug: leadmagnet/sales-page-hero
+ * Categories: banner
+ * Inserter: true
+ */
+?>
+
+<!-- wp:group {"metadata":{"name":"salesPageHeroSection"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|6","bottom":"var:preset|spacing|6"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--6);padding-bottom:var(--wp--preset--spacing--6)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|5"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"padding":{"left":"var:preset|spacing|8"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-left:var(--wp--preset--spacing--8)"><!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase"}}} -->
+<p style="text-transform:uppercase">leefstijs begeleiding aanhuis</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":1} -->
+<h1 class="wp-block-heading">Klachtenvrij leven zonder beperkingen!</h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Je hoeft niet meer naar een praktijk te reizen als dat te veel energie kost. Ik kom naar jou toe en help je in je eigen omgeving de juiste stappen te zetten naar herstel en balans.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"400"}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" style="font-style:normal;font-weight:400;text-transform:uppercase">plan je gratis intake</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
+
+<!-- wp:image {"id":48,"scale":"cover","sizeSlug":"large","linkDestination":"none","style":{"border":{"radius":{"topLeft":"150px"}},"layout":{"selfStretch":"fill","flexSize":null}}} -->
+<figure class="wp-block-image size-large has-custom-border"><img src="http://leadengine.local/wp-content/uploads/2025/12/trajecten-1024x681.webp" alt="vrouw die geniet van de zonsondergang" class="wp-image-48" style="border-top-left-radius:150px;object-fit:cover"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
