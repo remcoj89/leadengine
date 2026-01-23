88f464a1a207e7502fd99ee3cd44fc95922ca069
remcojonk <remcojonk@hotmail.com>
2026-01-23 15:13:21 +0700

    created the review section with background image

---
+++ patterns/section-flex-card-light.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Title: Section Flex Card Light
+ * Slug: leadmagnet/section-flex-card-light
+ * Description:A flex section with card style layout and light background.
+ * Categories: section
+ * Inserter: yes
+ */
+?>
+
+<!-- wp:generateblocks/element {"uniqueId":"1469b780","tagName":"div","styles":{"@media (max-width:1024px)":{"paddingLeft":"2.5rem","paddingRight":"2.5rem"}},"css":"@media (max-width:1024px){.gb-element-1469b780{padding-left:2.5rem;padding-right:2.5rem}}","metadata":{"name":"Flex Card Section"}} -->
+<div class="gb-element-1469b780">
+  <!-- wp:group {"layout":{"type":"constrained","contentSize":"","wideSize":""}} -->
+  <div class="wp-block-group">
+    <!-- wp:generateblocks/element {"uniqueId":"0ebeecdb","tagName":"div","styles":{"display":"grid","gridTemplateColumns":"repeat(2, minmax(0, 1fr))","columnGap":"2rem","rowGap":"2rem","@media (max-width:767px)":{"gridTemplateColumns":"1fr"},"backgroundColor":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dcolor\u002d\u002dsoft-linen, #F5F0E6)","paddingTop":"4rem","paddingBottom":"4rem","paddingRight":"4rem","paddingLeft":"4rem","borderTopLeftRadius":"16px","borderTopRightRadius":"16px","borderBottomRightRadius":"16px","borderBottomLeftRadius":"16px","width":"50%","@media (max-width:1024px)":{"gridTemplateColumns":"1fr","paddingLeft":"2.5rem","paddingRight":"2.5rem","paddingBottom":"2.5rem","paddingTop":"2.5rem","marginLeft":"2.5rem","marginRight":"2.5rem","width":"unset"}},"css":".gb-element-0ebeecdb{background-color:var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dcolor\u002d\u002dsoft-linen,#F5F0E6);column-gap:2rem;display:grid;grid-template-columns:repeat(2,minmax(0,1fr));row-gap:2rem;width:50%;border-radius:16px;padding:4rem}@media (max-width:1024px){.gb-element-0ebeecdb{grid-template-columns:1fr;margin-left:2.5rem;margin-right:2.5rem;width:unset;padding:2.5rem}}@media (max-width:767px){.gb-element-0ebeecdb{grid-template-columns:1fr}}"} -->
+    <div class="gb-element-0ebeecdb">
+      <!-- wp:generateblocks/element {"uniqueId":"b435338a","tagName":"div","styles":{"display":"grid","gridTemplateColumns":"1fr","rowGap":"1rem"},"css":".gb-element-b435338a{display:grid;grid-template-columns:1fr;row-gap:1rem}"} -->
+      <div class="gb-element-b435338a">
+        <!-- wp:heading -->
+        <h2 class="wp-block-heading">Wanneer klachten normaal lijken te worden</h2>
+        <!-- /wp:heading -->
+
+        <!-- wp:generateblocks/media {"uniqueId":"fd7bdc29","tagName":"img","styles":{"maxWidth":"100%","objectFit":"cover","width":"auto","borderTopLeftRadius":"16px","borderTopRightRadius":"16px","borderBottomRightRadius":"16px","borderBottomLeftRadius":"16px","alignItems":"stretch","justifyContent":"space-around","aspectRatio":"1/1"},"css":".gb-media-fd7bdc29{align-items:stretch;aspect-ratio:1/1;justify-content:space-around;max-width:100%;object-fit:cover;width:auto;border-radius:16px}","htmlAttributes":{"alt":"Uitegputte man met zijn gezich op het toetsenbord van zijn laptop","title":"burn-out","src":"http://leadengine.local/wp-content/uploads/2025/12/burn-out-scaled.webp"},"mediaId":36} -->
+        <img class="gb-media-fd7bdc29" alt="Uitegputte man met zijn gezich op het toetsenbord van zijn laptop" title="burn-out" src="http://leadengine.local/wp-content/uploads/2025/12/burn-out-scaled.webp"/>
+        <!-- /wp:generateblocks/media -->
+      </div>
+      <!-- /wp:generateblocks/element -->
+
+      <!-- wp:generateblocks/element {"uniqueId":"441e54cc","tagName":"div","styles":{"display":"block"},"css":".gb-element-441e54cc{display:block}"} -->
+      <div class="gb-element-441e54cc">
+        <!-- wp:paragraph -->
+        <p>Lorem ipsum dolor sit amet consectetur, adipiscing elit conubia fermentum cubilia sem, dis lobortis fames suspendisse. Morbi ultrices rhoncus himenaeos turpis iaculis suscipit aptent, montes odio pellentesque cursus semper fermentum tempus, sociis consequat at phasellus fusce sociosqu.<br></p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|ocean-mist"}}}},"textColor":"ocean-mist","fontSize":"lg"} -->
+        <p class="has-ocean-mist-color has-text-color has-link-color has-lg-font-size">"Lorem ipsum dolor sit amet consectetur, adipiscing elit conubia fermentum cubilia sem, dis lobortis fames suspendisse."<br></p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:paragraph -->
+        <p>Lorem ipsum dolor sit amet consectetur, adipiscing elit conubia fermentum cubilia sem, dis lobortis fames suspendisse. Morbi ultrices rhoncus himenaeos turpis iaculis suscipit aptent, montes odio pellentesque cursus semper fermentum tempus, sociis consequat at phasellus fusce sociosqu.<br></p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:generateblocks/element -->
+    </div>
+    <!-- /wp:generateblocks/element -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:generateblocks/element -->
---
+++ patterns/section-review-with-bg-image.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Title: Section Review With Background Image
+ * Slug: leadmagnet/section-review-with-bg-image
+ * Description: A review section with a background image and overlay.
+ * Categories: section
+ * Inserter: yes
+ */
+?>
+
+
+
+<!-- wp:generateblocks/element {"uniqueId":"a70b6805","tagName":"div","styles":{"backgroundImage":"linear-gradient(to left, rgba(245, 240, 230, 0.7) 0%, rgba(245, 240, 230, 0.7) 100%),url(http://leadengine.local/wp-content/uploads/2025/12/reviews-de-lijfstijl-scaled.webp)","backgroundSize":"cover,cover","backgroundRepeat":"no-repeat,no-repeat","backgroundPosition":"center,center","backgroundBlendMode":"normal,normal","paddingTop":"5rem","paddingBottom":"5rem"},"css":".gb-element-a70b6805{background-blend-mode:normal,normal;padding-bottom:5rem;padding-top:5rem;background:linear-gradient(to left,rgba(245,240,230,0.7) 0%,rgba(245,240,230,0.7) 100%),url(http://leadengine.local/wp-content/uploads/2025/12/reviews-de-lijfstijl-scaled.webp) center /cover no-repeat}","metadata":{"name":"Review Section With Background Image"}} -->
+<div class="gb-element-a70b6805">
+  <!-- wp:generateblocks/element {"uniqueId":"f3a2575b","tagName":"div","styles":{"backgroundColor":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dcolor\u002d\u002djet-black, #1B2C39)","color":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dcolor\u002d\u002dwhite, #FFFFFF)","paddingTop":"2rem","paddingRight":"2rem","paddingBottom":"2rem","paddingLeft":"2rem","borderTopLeftRadius":"16px","borderTopRightRadius":"16px","borderBottomRightRadius":"16px","borderBottomLeftRadius":"16px","maxWidth":"65ch","marginLeft":"auto","marginRight":"auto"},"css":".gb-element-f3a2575b{background-color:var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dcolor\u002d\u002djet-black,#1B2C39);color:var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dcolor\u002d\u002dwhite,#FFFFFF);margin-left:auto;margin-right:auto;max-width:65ch;border-radius:16px;padding:2rem}"} -->
+  <div class="gb-element-f3a2575b">
+    <!-- wp:heading {"textAlign":"center"} -->
+    <h2 class="wp-block-heading has-text-align-center">Succes verhalen</h2>
+    <!-- /wp:heading -->
+
+    <!-- wp:paragraph {"align":"center"} -->
+    <p class="has-text-align-center">Lorem ipsum dolor sit amet consectetur, adipiscing elit conubia fermentum cubilia sem, dis lobortis fames suspendisse. Morbi ultrices rhoncus himenaeos turpis iaculis suscipit aptent, montes odio pellentesque cursus semper fermentum tempus, sociis consequat at phasellus fusce sociosqu.</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:paragraph {"align":"center","style":{"typography":{"textTransform":"uppercase"}}} -->
+    <p class="has-text-align-center" style="text-transform:uppercase">- Naan van de tevreden klant</p>
+    <!-- /wp:paragraph -->
+  </div>
+  <!-- /wp:generateblocks/element -->
+</div>
+<!-- /wp:generateblocks/element -->
