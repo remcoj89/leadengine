302903e4a7eb5a2bfc6276190768cb5f73daac64
remcojonk <remcojonk@hotmail.com>
2026-01-27 13:14:23 +0700

    created the contact page including patterns

---
+++ patterns/section-flex-contact-form.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Title: Section Flex Contact Form
+ * Slug: leadmagnet/section-flex-contact-form
+ * Description: A flex section with contact form and map.
+ * Categories: section
+ * Inserter: yes
+ */
+?>
+
+<!-- wp:generateblocks/element {"uniqueId":"d16c28ce","tagName":"div","styles":{"paddingTop":"5rem","paddingBottom":"5rem","@media (max-width:1024px)":{"paddingLeft":"2.5rem","paddingRight":"2.5rem"},"@media (max-width:767px)":{"paddingLeft":"1rem","paddingRight":"1rem"}},"css":".gb-element-d16c28ce{padding-bottom:5rem;padding-top:5rem}@media (max-width:1024px){.gb-element-d16c28ce{padding-left:2.5rem;padding-right:2.5rem}}@media (max-width:767px){.gb-element-d16c28ce{padding-left:1rem;padding-right:1rem}}","metadata":{"name":"Contact Form Section"}} -->
+<div class="gb-element-d16c28ce">
+  <!-- wp:group {"layout":{"type":"constrained"}} -->
+  <div class="wp-block-group">
+    <!-- wp:generateblocks/element {"uniqueId":"d8c16a3e","tagName":"div","styles":{"display":"grid","gridTemplateColumns":"repeat(2, minmax(0, 1fr))","columnGap":"3rem","rowGap":"1em","@media (max-width:767px)":{"gridTemplateColumns":"1fr"},"alignItems":"stretch","@media (max-width:1024px)":{"gridTemplateColumns":"1fr"}},"css":".gb-element-d8c16a3e{align-items:stretch;column-gap:3rem;display:grid;grid-template-columns:repeat(2,minmax(0,1fr));row-gap:1em}@media (max-width:1024px){.gb-element-d8c16a3e{grid-template-columns:1fr}}@media (max-width:767px){.gb-element-d8c16a3e{grid-template-columns:1fr}}"} -->
+    <div class="gb-element-d8c16a3e">
+      <!-- wp:generateblocks/element {"uniqueId":"78ee5781","tagName":"div","styles":{"paddingTop":"2rem","paddingRight":"2rem","paddingBottom":"2rem","paddingLeft":"2rem","borderTopLeftRadius":"16px","borderTopRightRadius":"16px","borderBottomRightRadius":"16px","borderBottomLeftRadius":"16px","backgroundColor":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dcolor\u002d\u002dsoft-linen, #F5F0E6)"},"css":".gb-element-78ee5781{background-color:var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dcolor\u002d\u002dsoft-linen,#F5F0E6);border-radius:16px;padding:2rem}","metadata":{"name":"Contact form wrapper"}} -->
+      <div class="gb-element-78ee5781">
+        <!-- wp:heading -->
+        <h2 class="wp-block-heading">Neem contact met ons op</h2>
+        <!-- /wp:heading -->
+
+        <!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase"}}} -->
+        <p style="text-transform:uppercase">Lorem ipsum dolor</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:wpforms/form-selector {"clientId":"be08be06-4983-4b35-936c-882e18bcbb6e","formId":"317","theme":"default-copy-1-copy-2-copy-1-copy-2-copy-3-copy-1","themeName":"Default (Copy)","labelColor":"rgba(0, 0, 0, 0.85)","labelSublabelColor":"rgba(0, 0, 0, 0.55)","copyPasteJsonValue":"{\u0022displayTitle\u0022:false,\u0022displayDesc\u0022:false,\u0022theme\u0022:\u0022default-copy-1-copy-2-copy-1-copy-2-copy-3-copy-1\u0022,\u0022themeName\u0022:\u0022Default (Copy)\u0022,\u0022fieldSize\u0022:\u0022large\u0022,\u0022backgroundImage\u0022:\u0022none\u0022,\u0022backgroundPosition\u0022:\u0022center center\u0022,\u0022backgroundRepeat\u0022:\u0022no-repeat\u0022,\u0022backgroundSizeMode\u0022:\u0022cover\u0022,\u0022backgroundSize\u0022:\u0022cover\u0022,\u0022backgroundWidth\u0022:\u0022100px\u0022,\u0022backgroundHeight\u0022:\u0022100px\u0022,\u0022backgroundUrl\u0022:\u0022url()\u0022,\u0022backgroundColor\u0022:\u0022rgba( 0, 0, 0, 0 )\u0022,\u0022fieldBorderRadius\u0022:\u002216px\u0022,\u0022fieldBorderStyle\u0022:\u0022solid\u0022,\u0022fieldBorderSize\u0022:\u00220px\u0022,\u0022fieldBackgroundColor\u0022:\u0022#ffffff\u0022,\u0022fieldBorderColor\u0022:\u0022rgba(0, 0, 0, 0.25)\u0022,\u0022fieldTextColor\u0022:\u0022rgba(0, 0, 0, 0.7)\u0022,\u0022fieldMenuColor\u0022:\u0022#ffffff\u0022,\u0022labelSize\u0022:\u0022medium\u0022,\u0022labelColor\u0022:\u0022rgba(0, 0, 0, 0.85)\u0022,\u0022labelSublabelColor\u0022:\u0022rgba(0, 0, 0, 0.55)\u0022,\u0022labelErrorColor\u0022:\u0022#d63637\u0022,\u0022buttonSize\u0022:\u0022medium\u0022,\u0022buttonBorderStyle\u0022:\u0022none\u0022,\u0022buttonBorderSize\u0022:\u00221px\u0022,\u0022buttonBorderRadius\u0022:\u0022500px\u0022,\u0022buttonBackgroundColor\u0022:\u0022#6B4CE6\u0022,\u0022buttonTextColor\u0022:\u0022#ffffff\u0022,\u0022buttonBorderColor\u0022:\u0022#066aab\u0022,\u0022pageBreakColor\u0022:\u0022#066aab\u0022,\u0022containerPadding\u0022:\u00220px\u0022,\u0022containerBorderStyle\u0022:\u0022none\u0022,\u0022containerBorderWidth\u0022:\u00221px\u0022,\u0022containerBorderColor\u0022:\u0022#000000\u0022,\u0022containerBorderRadius\u0022:\u00223px\u0022,\u0022containerShadowSize\u0022:\u0022none\u0022,\u0022customCss\u0022:\u0022\u0022}","fieldSize":"large","fieldBorderSize":"0px","fieldBorderRadius":"16px","fieldBorderColor":"rgba(0, 0, 0, 0.25)","fieldTextColor":"rgba(0, 0, 0, 0.7)","buttonBorderRadius":"500px","buttonBackgroundColor":"#6B4CE6"} /--></div>
+        <!-- /wp:generateblocks/element -->
+
+        <!-- wp:generateblocks/element {"uniqueId":"aad9c8f0","tagName":"div","styles":{"maxWidth":"65ch","@media (max-width:1024px)":{"paddingLeft":"2.5rem","paddingRight":"2.5rem"},"@media (max-width:767px)":{"paddingLeft":"2rem","paddingRight":"2rem"},"alignItems":"stretch","justifyContent":"space-between","borderTopLeftRadius":"16px","borderTopRightRadius":"16px","borderBottomRightRadius":"16px","borderBottomLeftRadius":"16px","overflowX":"hidden","overflowY":"hidden"},"css":".gb-element-aad9c8f0{align-items:stretch;justify-content:space-between;max-width:65ch;overflow-x:hidden;overflow-y:hidden;border-radius:16px}@media (max-width:1024px){.gb-element-aad9c8f0{padding-left:2.5rem;padding-right:2.5rem}}@media (max-width:767px){.gb-element-aad9c8f0{padding-left:2rem;padding-right:2rem}}","className":"embed-map-container"} -->
+        <div class="gb-element-aad9c8f0 embed-map-container">
+          <!-- wp:html -->
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1592.8535897717463!2d4.888271587655132!3d52.60073738386028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5ff742d66170f%3A0xdb4c6f48fa2753a!2sWestend%20Digital!5e0!3m2!1sen!2s!4v1769493726803!5m2!1sen!2s" class="iframe" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <!-- /wp:html -->
+        </div>
+      <!-- /wp:generateblocks/element -->
+    </div>
+  <!-- /wp:generateblocks/element -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:generateblocks/element -->
